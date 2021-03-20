f48e00c2999b301134b47077beb0053ea337874a
nicoalanis14 <nicoalanis14@gmail.com>
2021-03-20 13:01:01 -0300

    landing finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,10 @@ const Footer = () => {
   return (
     <div className="container-footer">
       <h2>Send Me Mail</h2>
-      <input type="text"></input>
-      <button className="btn">Contact</button>
+      <div className="input-footer">
+        <input type="text" placeholder="example@gmail.com"></input>
+        <button className="btn">Contact</button>
+      </div>
     </div>
   );
 };
